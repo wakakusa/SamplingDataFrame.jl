fd3157448915ba1616a228480f042ebefa41fb00
wakakusa <a.beginnerof.kdd.english@gmail.com>
2016-10-02 21:59:38 +0900

    関数修正
    四捨五入の関数を非推奨から推奨に変更

--- src/SamplingDataFrame.jl
+++ src/SamplingDataFrame.jl
@@ -11,7 +11,7 @@ function TestDataSetsNumbering(DF::DataFrame,PERCENTAGE::Float64)
 
   #学習用データの行番号を求める
   #Num(100分率)抽出するため、抽出対象行を作成
-  TrainingNumbering=sample(1:size(DF, 1), iceil(PERCENTAGE * size(DF, 1)) ,replace=false, ordered=true)
+  TrainingNumbering=sample(1:size(DF, 1), Int(ceil(PERCENTAGE * size(DF, 1))) ,replace=false, ordered=true)
 
   #評価用データの行番号を求める
   TestNumbering=setdiff(FullNumbering,TrainingNumbering)
