0accca92dc98594228579544e9572ff8a2dd5d53
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-01 14:35:41 +0000

    build based on 310fec4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SamplingDataFrame","category":"page"},{"location":"#SamplingDataFrame","page":"Home","title":"SamplingDataFrame","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SamplingDataFrame]","category":"page"}]
+}
